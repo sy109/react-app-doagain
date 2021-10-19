bb4cdee31a1adde08b3894fdc901fec80d67ea8e
sy109 <s-yoon3@wiu.edu>
2021-10-19 14:51:56 +0900

    컴포넌트화 및 라우팅 간단 사용

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, useParams} from 'react-router-dom';
+import React,{useState} from 'react';
 
-
+function Header(){
+  return <h1><Link to="/">WEB</Link></h1>;
+}
+function Nav(props){
+  var lis = [];
+  var topics = props.topics;
+  var title, body;
+  for(var i = 0; i < topics.length; i ++){
+    lis.push(<li><Link to={"/read/" + topics[i].id}>{topics[i].title}</Link></li>);
+  }
+  return <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>;
+}
+function Article(props){
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>;
+}
+function Read(props){
+  var params = useParams();
+  var id = Number(params.id);
+  var title, body;
+  for (var i = 0; i< props.data.length; i++){
+    var topic = props.data[i];
+    if (topic.id === id){
+      title = topic.title;
+      body = topic.body;
+    }
+  }
+  return <Article title={title} body={body}></Article>
+}
 function App() {
-  function Header(){
-    return <h1><Link to="/">WEB</Link></h1>;
-  }
-  function Nav(){
-    return <nav>
-    <ol>
-      <li><Link to="/read/1">html</Link></li>
-      <li><Link to="/read/2">css</Link></li>
-    </ol>
-  </nav>;
-  }
-  function Article(){
-    return <article>
-    <h2>Welcome</h2>
-    Hello, React
-  </article>;
-  }
+  const [topics,setTopics] = useState([
+    { id: 1, title: "html", body: "html is ..."},
+    { id: 2, title: "css", body: "css is ..."}
+  ]);
+  
   return (
     <div>
       <Header></Header>
-      <Nav></Nav>
+      <Nav topics={topics}></Nav>
       
-      <Route exact path="/">Welcome</Route>
-      <Route path="/read/:id">Read</Route>
+      <Route exact path="/">
+        <Article title="Welcome" body="Hello, WEB"></Article>
+      </Route>
+      <Route path="/read/:id">
+        <Read data={topics}></Read>
+      </Route>
     </div>
   );
 }
